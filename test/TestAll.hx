ea87c59c853335852e2b95f1a7ce4fab30571146
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-10-29 21:06:56 -0600

    init project

---
+++ test/TestAll.hx
@@ -0,0 +1,17 @@
+import utest.UTest;
+
+import utest.Assert;
+
+class TestAll {
+  static function main() {
+    UTest.run([
+      new TestAll()
+    ]);
+  }
+
+  public function new() {}
+
+  public function testBasics() {
+
+  }
+}
