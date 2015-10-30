b60d7386bcb8490ad3d9f5a1ad9d273c3b34277b
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-10-30 08:38:28 -0600

    adding html dom features

--- src/doom/Doom.hx
+++ src/doom/Doom.hx
@@ -5,7 +5,30 @@ using thx.Strings;
 
 import doom.Node;
 
+#if (js && html)
+import js.html.*;
+import js.Browser.*;
+#end
+
 class Doom {
+#if (js && html)
+  public static function nodeToHtml(node : Node) return switch (node : NodeImpl) {
+    case Element(name, attributes, events, children):
+      var el = document.createElement(name);
+      for(key in attributes.keys())
+        el.setAttribute(key, attributes.get(key));
+      for(child in children) {
+        var n = nodeToHtml(child);
+        if(null != n)
+          el.appendChild(n);
+      }
+      el;
+    case Text(text): document.createTextNode(text);
+    case Comment(text): document.createComment(text);
+    case Empty: null;
+  }
+#end
+
   public static function nodeToXml(node : Node) return switch (node : NodeImpl) {
     case Element(name, attributes, events, children):
       var xml = Xml.createElement(name);
