c5b83a9d1c5fc034cd55422e63f7f2f21af07ea0
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-11-05 20:18:57 -0700

    fixed issues in Component

--- src/doom/Component.hx
+++ src/doom/Component.hx
@@ -4,7 +4,8 @@ import js.html.Node as DomNode;
 import doom.Node;
 
 class Component<T> {
-  var mountNode : DomNode;
+  var parent : DomNode;
+  var element : DomNode;
   var node : Node;
   public function new() {}
 
@@ -14,45 +15,49 @@ class Component<T> {
   public function update(state : T) : Void {
     var oldNode = node,
         newNode = view(state);
-    if(null == mountNode)
+    if(null == parent && null == element && null == oldNode)
       throw new thx.Error('component is not mounted');
     if(null == oldNode) {
-      var parent = mountNode.parentNode,
-          el = HtmlNode.toHtml(node);
-      parent.appendChild(el); // TODO meh
-      mountNode = el;
+      element = HtmlNode.toHtml(newNode);
+      if(parent.nodeType == DomNode.ELEMENT_NODE)
+        (cast parent : js.html.Element).innerHTML = "";
+      parent.appendChild(element);
     } else {
       var patches = oldNode.diff(newNode);
-      HtmlNode.applyPatches(patches, mountNode);
+      HtmlNode.applyPatches(patches, element);
     }
+    node = newNode;
   }
 
-  inline static var beforeMount = "create";
+  inline static var create = "create";
   public function view(state : T) : Node {
     node = render(state);
     switch node {
       case Element(_, _, events, _):
         function m(e : js.html.Event) {
-          mount(cast e.target);
+          // mount(cast e.target, true);
+          element = cast e.target;
+          if(null == parent)
+            parent = element.parentNode;
+          else
+            parent.appendChild(element);
         }
-        if(events.exists(beforeMount)) {
-          var old = events.get(beforeMount);
-          events.set(beforeMount, function(e) {
-            m(e);
+        if(events.exists(create)) {
+          var old = events.get(create);
+          events.set(create, function(e) {
             old(e);
+            m(e);
           });
         } else {
-          events.set(beforeMount, m);
+          events.set(create, m);
         }
       case _:
     }
     return node;
   }
 
-  public function mount(node : DomNode) {
-    mountNode = node;
-    //if(mountNode.nodeType == DomNode.ELEMENT_NODE)
-    //  (cast mountNode : js.html.Element).innerHTML = "";
+  public function mount(parent : DomNode) {
+    this.parent = parent;
     return this;
   }
 }
