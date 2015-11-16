78e81ffa98510339fee519cf5ab7180900a01f55
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-11-15 20:41:16 -0700

    updating sample to include components

--- demo/00.basics/Main.hx
+++ demo/00.basics/Main.hx
@@ -1,16 +1,60 @@
 import thx.Nil;
 import thx.Timer;
 import doom.View;
+import doom.Component;
 import js.Browser.*;
 using doom.Node;
 import doom.Node.*;
 
 class Main {
   public static function main() {
-    new Like(document.getElementById("content"), { liked : false });
+    new Todo(document.getElementById("content"), { items : [] });
   }
 }
 
+class Todo extends View<TodoListModel> {
+  public function appendItem(description : String) {
+
+  }
+
+  public function removeChecked() {
+
+  }
+
+  public function setTodoValues(todo : TodoItemModel) {
+
+  }
+
+  override function render() {
+    return el("div",
+      ["class" => "todo"],
+      [
+        comp(new TodoList(state)),
+        comp(new TodoForm(nil))
+      ]
+    );
+  }
+}
+
+class TodoList extends Component<TodoListModel> {
+  override function render() {
+    return Empty;
+  }
+}
+
+class TodoForm extends Component<Nil> {
+  override function render() {
+    return Empty;
+  }
+}
+
+class TodoItem extends Component<TodoItemModel> {
+  override function render() {
+    return Empty;
+  }
+}
+
+/*
 class Like extends View<LikeModel> {
   override function render() {
     var text = state.liked ? 'like' : "haven't liked";
@@ -23,7 +67,13 @@ class Like extends View<LikeModel> {
   function handleClick(_, liked)
     update({ liked : !liked});
 }
+*/
 
-typedef LikeModel = {
-  liked : Bool
+typedef TodoListModel = {
+  items : Array<TodoItemModel>
+}
+
+typedef TodoItemModel = {
+  checked : Bool,
+  description : String
 }
