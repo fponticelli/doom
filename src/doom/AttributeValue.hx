2e1ca733be9a7af81c30403edf673f058845f7a6
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-11-21 14:23:48 -0700

    added more attribute value helpers

--- src/doom/AttributeValue.hx
+++ src/doom/AttributeValue.hx
@@ -2,6 +2,8 @@ package doom;
 
 import js.html.Event;
 import js.html.Element;
+import thx.Ints;
+import thx.Floats;
 
 abstract AttributeValue(AttributeValueImpl) from AttributeValueImpl to AttributeValueImpl {
   @:from static public function fromString(s : String) : AttributeValue
@@ -32,6 +34,32 @@ abstract AttributeValue(AttributeValueImpl) from AttributeValueImpl to Attribute
       e.preventDefault();
       var input : T = cast e.target;
       f(input);
+    });
+
+  @:from static public function fromStringValueHandler(f : String -> Void) : AttributeValue
+    return EventAttribute(function(e : Event) {
+      e.preventDefault();
+      var value : String = dots.Dom.getValue(cast e.target);
+      f(value);
+    });
+
+  @:from static public function fromBoolValueHandler(f : Bool -> Void) : AttributeValue
+    return EventAttribute(function(e : Event) {
+      e.preventDefault();
+      var value : Bool = (cast e.target : js.html.InputElement).checked;
+      f(value);
+    });
+
+  @:from static public function fromIntValueHandler(f : Int -> Void) : AttributeValue
+    return fromStringValueHandler(function(s : String) {
+      if(Ints.canParse(s))
+        f(Ints.parse(s));
+    });
+
+  @:from static public function fromFloatValueHandler(f : Float -> Void) : AttributeValue
+    return fromStringValueHandler(function(s : String) {
+      if(Floats.canParse(s))
+        f(Floats.parse(s));
     });
 
   @:op(A==B)
