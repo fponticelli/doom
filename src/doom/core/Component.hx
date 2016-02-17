91ba67af16d38631b004a9d4c583252cd8b6b526
Franco Ponticelli <franco.ponticelli@gmail.com>
2016-02-17 13:27:22 -0700

    children can be null

--- src/doom/core/Component.hx
+++ src/doom/core/Component.hx
@@ -2,14 +2,14 @@ package doom.core;
 
 class Component<Props, El> {
   public var props(default, null) : Props;
-  public var children(default, null) : VNodes;
+  public var children(default, null) : Null<VNodes>;
   public var element(default, null) : El;
   public var isUnmounted(default, null) : Bool = false;
   public var apply(default, null) : VNode -> El -> Void;
 
   public function new(props : Props, ?children : VNodes) {
     this.props = props;
-    this.children = null == children ? [] : children;
+    this.children = children;
   }
 
   public function render() : VNode {
