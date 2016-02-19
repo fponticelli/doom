cb8312b92008bed602cc4ee858610fa981c9c948
Franco Ponticelli <franco.ponticelli@gmail.com>
2016-02-18 23:27:55 -0700

    more helpers

--- src/doom/core/VNodes.hx
+++ src/doom/core/VNodes.hx
@@ -3,21 +3,21 @@ package doom.core;
 import doom.core.VNode;
 
 @:forward(length, concat, copy, filter, indexOf, iterator, join, lastIndexOf,
-          map, slice)
+          map, push, slice)
 @:arrayAccess
-abstract VNodes(Array<VNode>) from Array<VNode> {
+abstract VNodes(Array<VNode>) from Array<VNode> to Array<VNode> {
   @:from inline public static function node(node : VNode) : VNodes
     return [node];
 
   @:from inline public static function nodeImpl(node : VNodeImpl) : VNodes
     return [(node : VNode)];
 
+  @:from
+  public static function comps<Prop, El>(comps : Array<Component<Prop, El>>) : VNodes
+    return comps.map(VNode.comp);
+
   @:from inline public static function nodesImpl(nodes : Array<VNodeImpl>) : VNodes
     return cast nodes;
-
-  @:from
-  inline public static function comps<Prop, El>(comps : Array<Component<Prop, El>>) : VNodes
-    return comps.map(VNode.comp);
 
   inline public static function comment(content : String) : VNodes
     return [VNode.comment(content)];
